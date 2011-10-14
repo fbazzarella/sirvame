f41be36b4a036b6daa638e628ec757c8e1a47203
Felipe Bazzarella <fbazzarella@gmail.com>
2011-10-13 21:05:16 -0300

    Popovers do formulário de nova conta.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,6 +5,10 @@ $(function(){
             .focus(function(){if($(this).val()==ph)$(this).val('').css('color', '#333')})
             .blur(function(){if(!$(this).val())$(this).val(ph).css('color', '#666')})})
 
+    $('input[type=text]', '.create-account').popover({placement: 'left', offset: 5, trigger: 'focus', html: true})
+    $('input[type=password]', '.create-account').popover({placement: 'above', offset: 20, trigger: 'focus', html: true})
+    $('label > label', '.create-account').popover({placement: 'left', offset: 5, trigger: 'hover', html: true})
+
 /*
     $('#teste').spin({
         lines:  15,     // The number of lines to draw
