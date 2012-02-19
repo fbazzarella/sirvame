0ee7cbb0d9118272ba0b0814a060cc41cda68ac0
Felipe Bazzarella <fbazzarella@gmail.com>
2012-02-19 15:53:48 -0200

    add basic auth to production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery
+
+  before_filter :login_required_for_production_view
+
+  def login_required_for_production_view
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |name, password|
+        name == 'root' && password == '1q2w3e4r'
+      end
+    end
+  end
 end
