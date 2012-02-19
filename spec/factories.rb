d16cb8a11e41dafbbe34f887981146daa184a300
Felipe Bazzarella <fbazzarella@gmail.com>
2012-02-19 15:09:09 -0200

    refactory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
 	factory :company do
 		name         'Padaria Ki-Sabor'
 		phone        '24 2452 2222'
-		address      'Rua Com. Antonio Jannuzzi, 42, A'
+		address      'Rua Com. Antonio Jannuzzi, 42, A, Belo Horizonte (Subida do Quartel)'
 		segment_list 'Padaria, Confeitaria'
 		product_list 'bolos, tortas, salgados, pão, pães'
 	end
