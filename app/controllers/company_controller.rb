8fccce9824c1be24cf86104a07dc1662f0157f4b
Felipe Bazzarella <fbazzarella@gmail.com>
2012-02-19 20:11:25 -0200

    add quicksando to improvised search results

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,5 +1,10 @@
 class CompanyController < ApplicationController
   def index
-  	@companies = Company.all
+  	@companies = Company.all.sort_by{rand}
+
+  	respond_to do |f|
+  		f.html
+  		f.js { render partial: 'search_results' }
+  	end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,6 +5,14 @@ $(function(){
             .focus(function(){if($(this).val()==ph)$(this).val('').css('color', '#333')})
             .blur(function(){if(!$(this).val())$(this).val(ph).css('color', '#666')})})
 
+    $('input#search').keypress(function(){
+        $.ajax({
+            url: '/search.js',
+            type: 'GET',
+            dataType: 'html',
+            success: function(data){
+                $('.search-results').quicksand($(data).find('li'), {adjustHeight: 'dynamic'})}})})
+
     // $('input[type=text]', '.create-account').popover({placement: 'left', offset: 5, trigger: 'focus', html: true})
     // $('input[type=password]', '.create-account').popover({placement: 'above', offset: 20, trigger: 'focus', html: true})
     // $('.payment-period', '.create-account').popover({placement: 'left', offset: 5, trigger: 'hover', html: true})
