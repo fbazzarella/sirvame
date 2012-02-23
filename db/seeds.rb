b9e3f7c38b2b5721b0e8bef7d1c369fd23285571
Felipe Bazzarella <fbazzarella@gmail.com>
2012-02-23 21:21:59 -0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,39 @@
 # encoding: utf-8
 Company.destroy_all
 
-10.times do
-	Company.create!([{
-		name:         'Padaria Ki-Sabor',
-		phone:        '24 2452 2222',
-		address:      'Rua Com. Antonio Jannuzzi, 42, A, Belo Horizonte (Subida do Quartel)',
-		segment_list: 'Padaria, Confeitaria',
-		product_list: 'bolos, tortas, salgados, pão, pães'
-	},{
-		name:         'RogeGuts',
-		phone:        '24 2452 3333',
-		address:      'Rua Cel. João Rufino, 123, Centro (Jardim de Cima)',
-		segment_list: 'Lanchonete, Restaurante, Sorveteria',
-		product_list: 'lanche, hamburger, sorvete, refeição, almoço, entrega'
-	},{
-		name:         'Drogaria Benfica',
-		phone:        '24 2452 7263',
-		address:      'Rua dos Mineiros, 122, B, Centro',
-		segment_list: 'Drogaria, Farmácia, Perfumaria',
-		product_list: 'cosméticos, perfumes, remédios, genéricos, farmácia popular'
-	}], as: :admin)
-end
+Company.create!([{
+	name:         'Carlão Academia',
+	phone:        '24 2453 3399',
+	segment_list: 'Academia, Musculação',
+	product_list: 'musculação, lutas, jiu-jitsu, judô'
+},{
+	name:         'Açougue Boi Bom',
+	phone:        '24 2453 4053',
+	segment_list: 'Açougue, Casa de Carne',
+	product_list: 'carnes, carvão, churrasco, linguiça'
+},{
+	name:         'Miliartes',
+	phone:        '24 2453 3123',
+	segment_list: 'Gráfica, Identidade, Visual',
+	product_list: 'adesivos, cartões, visita, panfletos, cartazes, impressões'
+},{
+	name:         'Adelino dos Santos',
+	phone:        '24 2452 3638',
+	segment_list: 'Advogado, Advocacia, Imóveis, Imobiliária',
+	product_list: 'criminal, criminalista, casas, apartamentos'
+},{
+	name:         'Etal Turismo',
+	phone:        '24 2453 4661',
+	segment_list: 'Turismo, Viagens',
+	product_list: 'passeio, viagem, excursão'
+},{
+	name:         'Casa das Antenas',
+	phone:        '24 2453 3358',
+	segment_list: 'Antenas, Alarmes',
+	product_list: 'segurança, portão, portões, sky, parabólicas'
+},{
+	name:         'Águas do Vale',
+	phone:        '24 2452 3864',
+	segment_list: 'Gás',
+	product_list: 'água'
+}], as: :admin)
