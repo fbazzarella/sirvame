7825fcd31f3624f1949b0ce055a513dd98c9fb4c
Felipe Bazzarella <fbazzarella@gmail.com>
2012-03-21 21:31:28 -0300

    fix js syntax again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,15 @@ $(function(){
     
     $('input[placeholder]').each(function(){
         var ph = $(this).attr('placeholder');
-        if(!$(this).val()) $(this).val(ph).css('color', '#666');
-        $(this).focus(function(){if($(this).val()==ph)$(this).val('').css('color', '#333')})
-            .blur(function(){if(!$(this).val())$(this).val(ph).css('color', '#666')})})
+
+        if(!$(this).val()){ $(this).val(ph).css('color', '#666') };
+
+        $(this).focus(function(){
+            if($(this).val()==ph){ $(this).val('').css('color', '#333') };
+        }).blur(function(){
+            if(!$(this).val()){ $(this).val(ph).css('color', '#666') };
+        });
+    });
 
     $('.search form').submit(function(e){
         e.preventDefault();
@@ -27,9 +33,12 @@ $(function(){
             type: 'GET',
             dataType: 'html',
             success: function(data){
-                $('.search-results').quicksand($(data).find('li'), {adjustHeight: 'dynamic'})}})})
+                $('.search-results').quicksand($(data).find('li'), {adjustHeight: 'dynamic'});
+            }
+        });
+    });
 
     // $('input[type=text]', '.create-account').popover({placement: 'left', offset: 5, trigger: 'focus', html: true})
     // $('input[type=password]', '.create-account').popover({placement: 'above', offset: 20, trigger: 'focus', html: true})
     // $('.payment-period', '.create-account').popover({placement: 'left', offset: 5, trigger: 'hover', html: true})
-})
+});
