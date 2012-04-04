aa9e4134d3669ca398ebc7a07d17c5c09fe1fd78
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-04 11:44:54 -0300

    filter terms based on blacklist of words

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,4 +3,10 @@ class Search < ActiveRecord::Base
 	attr_accessible :term_list
 	validates :term_list, presence: true
 	acts_as_taggable_on :terms
+
+	class << self
+		def filter_terms(terms = [])
+			terms - %w(da de do)
+		end
+	end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -9,4 +9,10 @@ describe Search do
 	describe "validations" do
 		it { should validate_presence_of(:term_list)}
 	end
+
+	describe "filter terms" do
+		it "should remove blacklist's words from search terms" do
+			Search.filter_terms(%w{should not include do}).should_not include('do')
+		end
+	end
 end
