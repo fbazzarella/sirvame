5a9bdbbcf9e3098afd5df92752bf03ed2d65164d
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-04 12:39:17 -0300

    refactory

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     'Encontre o que precisa - Sirva.me'
   end
 
-  def normalize_search_params(search_params = nil)
-  	search_params.gsub(/,|\s|\++/i, '+').gsub(/\++/i, ', ') unless search_params.nil?
+  def normalize_search_params(params = nil)
+  	params.gsub(/,|\s|\++/i, '+').gsub(/\++/i, ', ') unless params.nil?
   end
 end
