72ecc754370688895f129407687887c929999ecd
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-04 00:01:36 -0300

    fix other bug on ie

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,7 +36,7 @@ $(function(){
         sp = $(this).find('input').val();
         searchUrl = '/encontrar' + (sp ? '/' + sp : '');
 
-        SirvaMeRouting.navigate(normalizeParams(searchUrl, 'go'));
+        SirvaMeRouting.navigate('!' + normalizeParams(searchUrl, 'go'));
 
         $.ajax({
             url: searchUrl + '.js',
@@ -49,13 +49,13 @@ $(function(){
     });
 
     window.SirvaMeRouting = new (Backbone.Router.extend({
-        routes: {'encontrar/:encontrar': 'search'},
+        routes: {'!/encontrar/:encontrar': 'search'},
         search: function(searchParams){
             searchForm.find('input').val(normalizeParams(searchParams, 'back'));
             searchForm.submit();
         }
     }));
 
-    Backbone.history.start({pushState: true});
+    Backbone.history.start();
 
 });
