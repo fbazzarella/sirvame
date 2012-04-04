c431dd34c419a82e186230d1102fe2bf4be29341
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-04 18:20:28 -0300

    add another word to black list

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class Search < ActiveRecord::Base
-	BLACKLIST = %w(da das de do dos)
+	BLACKLIST = %w(e da das de do dos)
 	attr_accessible :term_list
 	validates :term_list, presence: true
 	acts_as_taggable_on :terms
