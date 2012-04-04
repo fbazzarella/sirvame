2c3dcbc1d2df80a60cd1cd623e37d6d3b22e83e6
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-04 16:10:12 -0300

    perform search by model

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,12 +1,23 @@
 # encoding: utf-8
 class Search < ActiveRecord::Base
+	BLACKLIST = %w(da de do)
 	attr_accessible :term_list
 	validates :term_list, presence: true
 	acts_as_taggable_on :terms
 
 	class << self
+		def bring_me_results_for(term_list = nil)
+			if filtered_terms = filter_terms(term_list)
+				create({term_list: filtered_terms})
+				Company.tagged_with(filtered_terms.split(', '), any: true, wild: true)
+			else
+				Company.random(12)
+			end
+		end
+
 		def filter_terms(terms = nil)
-			(terms || []) - %w(da de do)
+			terms = terms.try(:split, ', ')
+			terms ? (terms - BLACKLIST).join(', ') : nil
 		end
 	end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -10,13 +10,43 @@ describe Search do
 		it { should validate_presence_of(:term_list)}
 	end
 
-	describe "filter terms" do
-		it "should remove blacklist's words from search terms" do
-			Search.filter_terms(%w{should not include do}).should_not include('do')
+	describe "bring me results for" do
+		context "with term list" do
+			it "should save a search information" do
+				expect do
+					Search.bring_me_results_for('term, list')
+				end.to change(Search, :count).by(1)
+			end
+
+			let!(:company) { Factory(:company) }
+			let!(:company1) { Factory(:company, product_list: 'bike') }
+			let!(:company2) { Factory(:company, product_list: 'ball') }
+
+			it "should return companies filtered by term list" do
+				Search.bring_me_results_for('bike, ball').should include(company1, company2)
+				Search.bring_me_results_for('bike, ball').should_not include(company)
+			end
+
+			it "should return empty array if not found any company with term list" do
+				Search.bring_me_results_for('icecream').should == []
+			end
 		end
 
-		it "should return empty array if none terms passed" do
-			Search.filter_terms.should == []
+		context "without term list" do
+			it "should return maximum 12 random companies" do
+				10.times { Factory(:company) }
+				Search.bring_me_results_for.count.should <= 12
+			end
+		end
+	end
+
+	describe "filter terms" do
+		it "should remove blacklist's words from search terms" do
+			Search.filter_terms('should, not, include, do').should == 'should, not, include'
+		end
+
+		it "should return nil if none terms passed" do
+			Search.filter_terms.should be_nil
 		end
 	end
 end
