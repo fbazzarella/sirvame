6c853569fe22284fa2bfd02e04492cdb0e04782f
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-09 21:35:24 -0300

    refactory factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,6 @@ FactoryGirl.define do
 		phone        '24 2452 2222'
 		address      'Company Address, 11, 111'
 		segment_list 'Segment, List'
-		product_list 'product, list'
+		product_list 'product, tags'
 	end
 end
