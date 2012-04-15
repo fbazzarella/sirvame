335e6cf39c3ab7d69932faa24564ae920f472068
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-14 22:45:08 -0300

    remove basic auth from production env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery
 
-  before_filter :login_required_for_production_view
+  before_filter :login_required_for_staging_view
 
-  def login_required_for_production_view
-    if Rails.env.production? || Rails.env.staging?
+  def login_required_for_staging_view
+    if Rails.env.staging?
       authenticate_or_request_with_http_basic do |name, password|
         name == 'root' && password == 'qwe123'
       end
