e11099f4097dc4830fa3d85fa87d0d9dc46a08de
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-28 03:32:15 -0300

    remove email from typus

--- app/controllers/admin/emails_controller.rb
+++
@@ -1,2 +0,0 @@
-class Admin::EmailsController < Admin::ResourcesController
-end
--- app/models/email.rb
+++
@@ -1,4 +0,0 @@
-class Email < ActiveRecord::Base
-	attr_accessible :name, :email, as: :admin
-	validates :name, :email, presence: true
-end
