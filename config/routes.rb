6b20b9f55068671fb2fc735d6b2557ef31c8a5c8
Felipe Bazzarella <fbazzarella@gmail.com>
2012-04-28 18:54:46 -0300

    add company show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SirvaMe::Application.routes.draw do
   root                           to: 'company#index'
   get '/encontrar(/:encontrar)', to: 'company#index'
+  get '/:company(/:page)',       to: 'company#show', as: :company
 end
--- spec/routing/company_routing_spec.rb
+++ spec/routing/company_routing_spec.rb
@@ -10,5 +10,15 @@ describe CompanyController do
     it "should search path routes to index" do
       get('/encontrar').should route_to('company#index')
     end
+
+    it "should company path routes do show" do
+    	get('/company').should route_to('company#show', company: 'company')
+    	company_path(:company).should == '/company'
+    end
+
+    it "should company page path routes do show" do
+    	get('/company/page').should route_to('company#show', company: 'company', page: 'page')
+    	company_path(:company, :page).should == '/company/page'
+    end
   end
 end
