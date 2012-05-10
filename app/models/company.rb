e782ad5917276035822cec1b942872a5b109f0d2
Felipe Bazzarella <fbazzarella@gmail.com>
2012-05-10 19:55:43 -0300

    add plan to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,13 @@
 class Company < ActiveRecord::Base
-	attr_accessible :name, :phone, :address, :segments, :products, :username, :plus, as: :admin
+  PLANS = %w(none plus)
+
+	attr_accessible :name, :phone, :address, :segments, :products, :username, :plus, :plan, as: :admin
+
 	validates :name, :phone, :segments, presence: true
   validates :username, format: {with: /\A[-_\.a-z0-9]+\Z/}, allow_blank: true
+  validates :plan, inclusion: {in: PLANS}
+
+  def self.plans # Just for Typus
+    PLANS
+  end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Company do
 	describe "mass assignment security" do
-		[:name, :phone, :address, :segments, :products, :username, :plus].each do |attr|
+		[:name, :phone, :address, :segments, :products, :username, :plus, :plan].each do |attr|
 			it { should allow_mass_assignment_of(attr).as(:admin) }
 		end
 	end
@@ -16,5 +16,9 @@ describe Company do
 		it { should allow_value('user-name_1.2').for(:username) }
 		it { should_not allow_value('user+name').for(:username) }
 		it { should_not allow_value('Username').for(:username) }
+
+		it { should allow_value('none').for(:plan) }
+		it { should allow_value('plus').for(:plan) }
+		it { should_not allow_value('other').for(:plan) }
 	end
 end
