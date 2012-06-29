47e811efe48f99eddaf4c26ca72a1555878072fb
Felipe Bazzarella <fbazzarella@gmail.com>
2012-06-28 22:27:32 -0300

    refactory config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SirvaMe::Application.config.session_store :cookie_store, key: '_sirvame_session'
+SirvaMe::Application.config.session_store :cookie_store, key: '_sirva_me_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
