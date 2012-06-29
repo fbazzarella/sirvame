22a0283176d006b5705b26164f283d774b6dc058
Felipe Bazzarella <fbazzarella@gmail.com>
2012-06-28 22:47:41 -0300

    refactory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,14 @@
-require 'capybara/rspec'
-
 ENV["RAILS_ENV"] ||= "test"
+
 require File.expand_path("../../config/environment", __FILE__)
-require "rspec/rails"
+require 'rspec/rails'
+require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
-  config.mock_with :rspec
   config.before(:suite) { DatabaseCleaner.strategy = :truncation }
-  config.before(:each) { DatabaseCleaner.start }
-  config.after(:each) { DatabaseCleaner.clean }
+  config.before(:each)  { DatabaseCleaner.start }
+  config.after(:each)   { DatabaseCleaner.clean }
 end
 
 ActiveSupport::Dependencies.clear
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
