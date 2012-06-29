466cbdf3c50a07aa3b5beda81d0da4721fc0ddb9
Felipe Bazzarella <fbazzarella@gmail.com>
2012-06-28 21:16:03 -0300

    refactory specs

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -26,32 +26,30 @@ describe Company do
 		it { should_not allow_value('other').for(:plan) }
 	end
 
+	let(:has_no_page)     { FactoryGirl.create(:company, plan: 'none') }
+	let(:has_no_page_too) { FactoryGirl.create(:company, plan: 'plus', username: '') }
+	let(:has_page)        { FactoryGirl.create(:company, plan: 'plus', username: 'company-name') }
+
 	describe "scopes" do
 		describe "all to sitemap" do
-			let!(:not_able_to_show) { FactoryGirl.create(:company, plan: 'none') }
-			let!(:not_able_to_show_too) { FactoryGirl.create(:company, plan: 'plus', username: '') }
-			let!(:able_to_show) { FactoryGirl.create(:company, plan: 'plus', username: 'company-name') }
+			subject { Company.all_to_sitemap }
 
-			it "should return any companies able to show on sitemap" do
-				Company.all_to_sitemap.should_not include(not_able_to_show)
-				Company.all_to_sitemap.should_not include(not_able_to_show_too)
-				Company.all_to_sitemap.should include(able_to_show)
-			end
+			it { should_not include(has_no_page) }
+			it { should_not include(has_no_page_too) }
+			it { should include(has_page) }
 		end
 	end
 
-	describe "have page" do
-		let!(:has_no_page) { FactoryGirl.create(:company, plan: 'none') }
-		let!(:has_no_page_too) { FactoryGirl.create(:company, plan: 'plus', username: '') }
-		let!(:has_page) { FactoryGirl.create(:company, plan: 'plus', username: 'company-name') }
+	describe "instance methods" do
+		describe "have page" do
+			it "should return false if company is not a paying" do
+				has_no_page.have_page?.should be_false
+				has_no_page_too.have_page?.should be_false
+			end
 
-		it "should return false if company is not a paying" do
-			has_no_page.have_page?.should be_false
-			has_no_page_too.have_page?.should be_false
-		end
-
-		it "should return true if company is a paying" do
-			has_page.have_page?.should be_true
+			it "should return true if company is a paying" do
+				has_page.have_page?.should be_true
+			end
 		end
 	end
 end
