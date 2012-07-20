9c1611fa816df732c9a042f93b8c9a48e057793c
Felipe Bazzarella <fbazzarella@gmail.com>
2012-07-19 23:58:55 -0300

    refactory

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -24,16 +24,16 @@ describe Search do
 		let!(:company3) { FactoryGirl.create(:company, products: 'toys') }
 		let!(:company4) { FactoryGirl.create(:company, segments: 'Movies', plan: 'plus') }
 
-		context "without term list" do
+		context "without word list" do
 			it "should return maximum 12 random companies" do
 				Search.perform_with.count.should <= 12
 			end
 		end
 
-		context "with term list" do
+		context "with word list" do
 			it "should save a search information" do
 				expect do
-					Search.perform_with('term list')
+					Search.perform_with('word list')
 				end.to change(Search, :count).by(1)
 			end
 
@@ -46,12 +46,12 @@ describe Search do
 				search_tag.name.should == 'word'
 			end
 
-			it "should return companies filtered by term list" do
+			it "should return companies filtered by word list" do
 				Search.perform_with('bike ball').should include(company1, company2)
 				Search.perform_with('bike ball').should_not include(company)
 			end
 
-			it "should return companies filtered by term list ignoring accents" do
+			it "should return companies filtered by word list ignoring accents" do
 				Search.perform_with('petít').should include(company)
 				Search.perform_with('marche').should include(company)
 			end
@@ -65,7 +65,7 @@ describe Search do
 				Search.perform_with('movies toys').should == [company4, company3]
 			end
 
-			it "should return empty array if not found any company with term list" do
+			it "should return empty array if not found any company with word list" do
 				Search.perform_with('icecream').should be_a(Array)
 				Search.perform_with('icecream').should be_empty
 			end
