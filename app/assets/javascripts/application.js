f2d5be14a95927b346953ad382559be1eb27cc2a
Felipe Bazzarella <fbazzarella@gmail.com>
2012-07-28 15:37:42 -0300

    fix indentation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,90 +5,90 @@
 
 $(function(){
 
-    function checkLionbars(){
-        $('#search-results .data').lionbars() }
+  function checkLionbars(){
+    $('#search-results .data').lionbars() }
 
-    function checkPlaceHolders(){
-        $('input[placeholder]').each(function(){
-            var ph = $(this).attr('placeholder');
+  function checkPlaceHolders(){
+    $('input[placeholder]').each(function(){
+      var ph = $(this).attr('placeholder');
 
-            if(!$(this).val()){ $(this).val(ph).css('color', '#666') };
+      if(!$(this).val()){ $(this).val(ph).css('color', '#666') };
 
-            $(this).bind('focus click', function(){
-                if($(this).val()==ph){ $(this).val('').css('color', '#333') }
-            }).blur(function(){
-                if(!$(this).val()){ $(this).val(ph).css('color', '#666') } }) }) };
+      $(this).bind('focus click', function(){
+        if($(this).val()==ph){ $(this).val('').css('color', '#333') }
+      }).blur(function(){
+        if(!$(this).val()){ $(this).val(ph).css('color', '#666') } }) }) };
 
-    function normalizeParams(params, direction){
-        if(!params) return '';
+  function normalizeParams(params, direction){
+    if(!params) return '';
 
-        params = decodeURI(params.replace(/,|\s|\++/gi, '+')).split('+');
+    params = decodeURI(params.replace(/,|\s|\++/gi, '+')).split('+');
 
-        for(i = params.length; i >= 0; i--){
-            if(!params[i]){ params.splice(i, 1) } };
+    for(i = params.length; i >= 0; i--){
+      if(!params[i]){ params.splice(i, 1) } };
 
-        switch(direction){
-            case 'go':   return params.join('+');
-            case 'back': return params.join(' ') } };
+    switch(direction){
+      case 'go':   return params.join('+');
+      case 'back': return params.join(' ') } };
 
-    var searchForm = $('.search form');
-    var spinContainer = searchForm.find('.add-on');
-    var zoomTool = spinContainer.html();
+  var searchForm = $('.search form');
+  var spinContainer = searchForm.find('.add-on');
+  var zoomTool = spinContainer.html();
 
-    searchForm.submit(function(e){
-        e.preventDefault();
+  searchForm.submit(function(e){
+    e.preventDefault();
 
-        firstLoad = false;
+    firstLoad = false;
 
-        var searchField = $(this).find('input');
+    var searchField = $(this).find('input');
 
-        if(searchField.val() != searchField.attr('placeholder')){
-            var spinner = new Spinner({
-                lines: 15, length: 5, width: 2, radius: 6, color: '#000',
-                speed: 1, trail: 30, shadow: false }).spin(spinContainer.empty()[0]);
+    if(searchField.val() != searchField.attr('placeholder')){
+      var spinner = new Spinner({
+        lines: 15, length: 5, width: 2, radius: 6, color: '#000',
+        speed: 1, trail: 30, shadow: false }).spin(spinContainer.empty()[0]);
 
-            var sp = normalizeParams(searchField.val(), 'go');
-            var searchUrl = '/encontrar' + (sp ? '/' + sp : '');
-            var routerUrl = sp ? '!/encontrar/' + sp : '';
+      var sp = normalizeParams(searchField.val(), 'go');
+      var searchUrl = '/encontrar' + (sp ? '/' + sp : '');
+      var routerUrl = sp ? '!/encontrar/' + sp : '';
 
-            SirvaMeRouting.navigate(routerUrl);
+      SirvaMeRouting.navigate(routerUrl);
 
-            $.ajax({
-                url: encodeURI(searchUrl + '.js'),
-                type: 'GET',
-                dataType: 'html',
-                success: function(data){
-                    $('#search-results').quicksand($(data).find('li'), {adjustHeight: 'dynamic'}, function(){
-                        checkLionbars();
-                        spinContainer.html(zoomTool) }) } }) } });
+      $.ajax({
+        url: encodeURI(searchUrl + '.js'),
+        type: 'GET',
+        dataType: 'html',
+        success: function(data){
+          $('#search-results').quicksand($(data).find('li'), {adjustHeight: 'dynamic'}, function(){
+            checkLionbars();
+            spinContainer.html(zoomTool) }) } }) } });
 
-    window.SirvaMeRouting = new (Backbone.Router.extend({
-        routes: {
-            '':                       'home',
-            '!/encontrar':            'search',
-            '!/encontrar/:encontrar': 'search' },
+  window.SirvaMeRouting = new (Backbone.Router.extend({
+    routes: {
+      '':                       'home',
+      '!/encontrar':            'search',
+      '!/encontrar/:encontrar': 'search' },
 
-        home: function(){
-            searchForm.find('input').blur().val('');
-            if(!firstLoad){ searchForm.submit() }
-            checkPlaceHolders() },
+    home: function(){
+      searchForm.find('input').blur().val('');
+      if(!firstLoad){ searchForm.submit() }
+      checkPlaceHolders() },
 
-        search: function(searchParams){
-            searchForm.find('input').blur().val(normalizeParams(searchParams, 'back'));
-            searchForm.submit();
-            checkPlaceHolders() } }) );
+    search: function(searchParams){
+      searchForm.find('input').blur().val(normalizeParams(searchParams, 'back'));
+      searchForm.submit();
+      checkPlaceHolders() } }) );
 
-    window.firstLoad = true;
+  window.firstLoad = true;
 
-    checkLionbars();
-    checkPlaceHolders();
+  checkLionbars();
+  checkPlaceHolders();
 
-    Backbone.history.start();
+  Backbone.history.start();
 
-    $('.addthis').show();
+  $('.addthis').show();
 
-    $('.cover a, .images a').fancybox({
-        openEffect: 'elastic', closeEffect: 'fade', prevEffect: 'fade', nextEffect: 'fade',
-        padding: 2, helpers: {title: {type: 'over'}} });
+  $('.cover a, .images a').fancybox({
+    openEffect: 'elastic', closeEffect: 'fade', prevEffect: 'fade', nextEffect: 'fade',
+    padding: 2, helpers: {title: {type: 'over'}} });
 
 });
