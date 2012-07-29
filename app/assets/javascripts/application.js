0cfaf5672e83f32db13febcfa56aa4c922a90d98
Felipe Bazzarella <fbazzarella@gmail.com>
2012-07-28 21:35:58 -0300

    js refactory

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,56 +2,32 @@
 //= require jquery-lionbars
 //= require fancybox
 //= require_tree ./plugins/
+//= require helpers
 
 $(function(){
 
-  function checkLionbars(){
-    $('#search-results .data').lionbars() }
-
-  function checkPlaceHolders(){
-    $('input[placeholder]').each(function(){
-      var ph = $(this).attr('placeholder');
-
-      if(!$(this).val()){ $(this).val(ph).css('color', '#666') };
-
-      $(this).bind('focus click', function(){
-        if($(this).val()==ph){ $(this).val('').css('color', '#333') }
-      }).blur(function(){
-        if(!$(this).val()){ $(this).val(ph).css('color', '#666') } }) }) };
-
-  function normalizeParams(params, direction){
-    if(!params) return '';
-
-    params = decodeURI(params.replace(/,|\s|\++/gi, '+')).split('+');
-
-    for(i = params.length; i >= 0; i--){
-      if(!params[i]){ params.splice(i, 1) } };
-
-    switch(direction){
-      case 'go':   return params.join('+');
-      case 'back': return params.join(' ') } };
-
-  var searchForm = $('.search form');
-  var spinContainer = searchForm.find('.add-on');
-  var zoomTool = spinContainer.html();
+  firstLoad     = true;
+  searchForm    = $('.search form');
+  searchField   = searchForm.find('input');
+  spinContainer = searchForm.find('.add-on');
+  zoomTool      = spinContainer.html();
+  searchUrl     = '/encontrar';
+  spinOptions   = {
+    lines: 15, length: 5, width: 2, radius: 6, color: '#000',
+    speed: 1, trail: 30, shadow: false };
 
   searchForm.submit(function(e){
     e.preventDefault();
 
     firstLoad = false;
 
-    var searchField = $(this).find('input');
-
     if(searchField.val() != searchField.attr('placeholder')){
-      var spinner = new Spinner({
-        lines: 15, length: 5, width: 2, radius: 6, color: '#000',
-        speed: 1, trail: 30, shadow: false }).spin(spinContainer.empty()[0]);
+      var spinner = new Spinner(spinOptions).spin(spinContainer.empty()[0]);
+      var sp      = normalizeParams(searchField.val(), 'go');
 
-      var sp = normalizeParams(searchField.val(), 'go');
-      var searchUrl = '/encontrar' + (sp ? '/' + sp : '');
-      var routerUrl = sp ? '!/encontrar/' + sp : '';
+      SirvaMeRouting.navigate(sp ? '!/encontrar/' + sp : '');
 
-      SirvaMeRouting.navigate(routerUrl);
+      searchUrl = '/encontrar' + (sp ? '/' + sp : '');
 
       $.ajax({
         url: encodeURI(searchUrl + '.js'),
@@ -62,7 +38,7 @@ $(function(){
             checkLionbars();
             spinContainer.html(zoomTool) }) } }) } });
 
-  window.SirvaMeRouting = new (Backbone.Router.extend({
+  SirvaMeRouting = new (Backbone.Router.extend({
     routes: {
       '':                       'home',
       '!/encontrar':            'search',
@@ -78,18 +54,12 @@ $(function(){
       searchForm.submit();
       checkPlaceHolders() } }) );
 
-  window.firstLoad = true;
-
   checkLionbars();
   checkPlaceHolders();
-
+  loadFancyBox();
   Backbone.history.start();
 
   $('.addthis').show();
   $('.pagination').remove();
-
-  $('.cover a, .images a').fancybox({
-    openEffect: 'elastic', closeEffect: 'fade', prevEffect: 'fade', nextEffect: 'fade',
-    padding: 2, helpers: {title: {type: 'over'}} });
 
 });
---
+++ app/assets/javascripts/helpers.js
@@ -0,0 +1,30 @@
+function checkLionbars(){
+  $('#search-results .data').lionbars() }
+
+function checkPlaceHolders(){
+  $('input[placeholder]').each(function(){
+    var ph = $(this).attr('placeholder');
+
+    if(!$(this).val()){ $(this).val(ph).css('color', '#666') };
+
+    $(this).bind('focus click', function(){
+      if($(this).val()==ph){ $(this).val('').css('color', '#333') }
+    }).blur(function(){
+      if(!$(this).val()){ $(this).val(ph).css('color', '#666') } }) }) };
+
+function loadFancyBox(){
+  $('.cover a, .images a').fancybox({
+    openEffect: 'elastic', closeEffect: 'fade', prevEffect: 'fade', nextEffect: 'fade',
+    padding: 2, helpers: {title: {type: 'over'}} }) };
+
+function normalizeParams(params, direction){
+  if(!params) return '';
+
+  params = decodeURI(params.replace(/,|\s|\++/gi, '+')).split('+');
+
+  for(i = params.length; i >= 0; i--){
+    if(!params[i]){ params.splice(i, 1) } };
+
+  switch(direction){
+    case 'go':   return params.join('+');
+    case 'back': return params.join(' ') } };
