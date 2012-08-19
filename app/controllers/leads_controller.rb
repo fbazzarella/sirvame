a8519f6e6aaf628b67ae66e9e66b735afa45f039
Felipe Bazzarella <fbazzarella@gmail.com>
2012-08-18 23:54:16 -0300

    create a lead

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -3,6 +3,11 @@ class LeadsController < ApplicationController
     @lead = Lead.new
   end
 
+  def create
+    @lead = Lead.new(params[:lead])
+    @lead.save ? redirect_to(effectived_lead_path) : render(:new)
+  end
+
   def effectived
   end
 end
--- spec/controllers/leads_controller_spec.rb
+++ spec/controllers/leads_controller_spec.rb
@@ -51,4 +51,50 @@ describe LeadsController do
       end
     end
   end
+
+  describe "POST create" do
+    it "should assign a new lead" do
+      FactoryGirl.build(:lead)
+
+      post :create
+
+      assigns(:lead).should be_a(Lead)
+      assigns(:lead).should_not be_persisted
+    end
+
+    context "with valid params" do
+      before { post :create, lead: FactoryGirl.attributes_for(:lead) }
+
+      it "should create a new lead" do
+        Lead.count.should == 1
+      end
+
+      it "should redirect to effectived path" do
+        response.should redirect_to(effectived_lead_path)
+      end
+    end
+
+    context "with invalid params" do
+      before { post :create, lead: FactoryGirl.attributes_for(:lead, email: 'email@example') }
+
+      it "should not create a new lead" do
+        Lead.count.should be_zero
+      end
+
+      it "should return success" do
+        response.should be_success
+      end
+
+      it "should render the new template with layout" do
+        response.should render_template('new')
+        response.should render_with_layout('application')
+      end
+
+      it "should raise missing template if invalid format is passed" do
+        expect do
+          post :create, format: :xml
+        end.to raise_error(ActionView::MissingTemplate)
+      end
+    end
+  end
 end
