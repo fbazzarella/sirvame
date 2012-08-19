899123ff6a414985b137b5a7bf0c5c5d23532b28
Felipe Bazzarella <fbazzarella@gmail.com>
2012-08-19 00:41:25 -0300

    lead views specs

---
+++ spec/requests/leads_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Lead do
+  before { visit new_lead_path }
+
+  it "should display effectived lead page with valid params" do
+    fill_in 'lead_company_name', with: 'Company Name'
+    fill_in 'lead_contact_name', with: 'Contact Name'
+    fill_in 'lead_email',        with: 'email@example.com'
+    click_button 'Quero Experimentar »'
+
+    page.should have_content('Sábia decisão :D')
+  end
+
+  it "should display new page with validation errors" do
+    click_button 'Quero Experimentar »'
+
+    page.should have_content('Este campo deve ser preenchido.')
+  end
+end
