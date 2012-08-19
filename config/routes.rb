c9eddbac786c29f0a16801f977dbfbdcb316208a
Felipe Bazzarella <fbazzarella@gmail.com>
2012-08-18 22:14:24 -0300

    lead effectived

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 SirvaMe::Application.routes.draw do
   root to: 'companies#index'
 
-  get '/anunciar-gratis-por-30-dias', to: 'leads#new',       as: :new_lead
+  get '/anunciar-gratis-por-30-dias',               to: 'leads#new', as: :new_lead
+  get '/anunciar-gratis-por-30-dias/sabia-decisao', to: 'leads#new', as: :effective_lead
   
-  get '/sitemap',                     to: 'pages#sitemap',   as: :sitemap
-  get '/robots',                      to: 'pages#robots',    as: :robots
+  get '/sitemap',                to: 'pages#sitemap',   as: :sitemap
+  get '/robots',                 to: 'pages#robots',    as: :robots
 
-  get '/encontrar(/:encontrar)',      to: 'companies#index', as: :companies
-  get '/:company',                    to: 'companies#show',  as: :company
+  get '/encontrar(/:encontrar)', to: 'companies#index', as: :companies
+  get '/:company',               to: 'companies#show',  as: :company
 end
