469b5e55958fdaf71695277c1e835f6aa893ab02
Felipe Bazzarella <fbazzarella@gmail.com>
2012-11-19 17:47:30 -0200

    add fix phone mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,14 @@
+# encoding: utf-8
 class NotificationMailer < ActionMailer::Base
   default from: '"Sirva.me Apps" <webbynode-apps@sirva.me>'
 
   def new_lead
     mail to: '"Atendimento Sirva.me" <fale-com@sirva.me>', subject: 'Novo Lead!'
+  end
+
+  def fix_phone(sugestion)
+    @sugestion = sugestion
+    
+    mail to: '"Atendimento Sirva.me" <fale-com@sirva.me>', subject: 'Nova Sugestão de Correção!'
   end
 end
--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require "spec_helper"
 
 describe NotificationMailer do
@@ -18,6 +19,30 @@ describe NotificationMailer do
 
     it "should set correct body" do
       mailer.body.to_s.should be_empty
+    end
+
+    it "should be added to the delivery queue" do
+      lambda { mailer.deliver }.should change(ActionMailer::Base.deliveries, :size).by(1)
+    end
+  end
+
+  describe "fix_phone" do
+    let(:mailer) { NotificationMailer.fix_phone({id: 1, name: 'Name', phone: '(12) 3456-7890'}) }
+
+    it "should set correct header From" do
+      mailer.header['From'].to_s.should == '"Sirva.me Apps" <webbynode-apps@sirva.me>'
+    end
+
+    it "should set correct header To" do
+      mailer.header['To'].to_s.should == '"Atendimento Sirva.me" <fale-com@sirva.me>'
+    end
+
+    it "should set correct subject" do
+      mailer.subject.should == 'Nova Sugestão de Correção!'
+    end
+
+    it "should set correct body" do
+      mailer.body.to_s.should include('1', 'Name', '(12) 3456-7890')
     end
 
     it "should be added to the delivery queue" do
