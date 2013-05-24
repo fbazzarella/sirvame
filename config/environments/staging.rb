c6cd3f7ea6f07f6bff3736ebb24af767d91926a5
Felipe Bazzarella <fbazzarella@gmail.com>
2013-05-24 09:45:36 -0300

    change config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,10 +11,9 @@ SirvaMe::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :yui
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
