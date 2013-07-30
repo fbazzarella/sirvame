533c7ab3f27e70b2493c42c3fa7349ad2cd59026
Felipe Bazzarella <fbazzarella@gmail.com>
2013-07-30 16:47:17 +0000

    fix indentation

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,21 +1,21 @@
 class Search < ActiveRecord::Base
-	has_and_belongs_to_many :companies
-
-	attr_accessible :word_list, :companies, as: :search
-
-	validates :word_list, presence: true
-
-	acts_as_taggable_on :words
-
-	def self.perform_with(word_list = nil, page = nil)
-		if word_list
-	    results = Company.search(word_list).order('plan DESC', 'relevance DESC', :name)
-
-			create({word_list: word_list.gsub(/\s/, ', '), companies: results}, as: :search) if page.nil?
-
-			results
-		else
-			Company.all_to_home.order('plan DESC', 'popularity DESC', :name)
-		end.page(page)
-	end
+  has_and_belongs_to_many :companies
+  
+  attr_accessible :word_list, :companies, as: :search
+  
+  validates :word_list, presence: true
+  
+  acts_as_taggable_on :words
+  
+  def self.perform_with(word_list = nil, page = nil)
+    if word_list
+      results = Company.search(word_list).order('plan DESC', 'relevance DESC', :name)
+      
+      create({word_list: word_list.gsub(/\s/, ', '), companies: results}, as: :search) if page.nil?
+      
+      results
+    else
+      Company.all_to_home.order('plan DESC', 'popularity DESC', :name)
+    end.page(page)
+  end
 end
