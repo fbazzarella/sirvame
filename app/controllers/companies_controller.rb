8bd9d42e2096a0a39abd4da1138e42bbc28c7240
Felipe Bazzarella <fbazzarella@gmail.com>
2013-07-30 16:48:56 +0000

    fix indentation

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,23 +1,23 @@
 class CompaniesController < ApplicationController
   def index
     session[:back_to_catalog_path] = back_to_catalog_path(params)
-
+    
     @companies = Search.perform_with(sanitize(params[:encontrar], :go), params[:page])
-
-  	respond_to do |f|
-  		f.html
-  		f.js   { render partial: 'search_results' }
-  		f.json { render json:    @companies.to_json }
-  	end
+    
+    respond_to do |f|
+      f.html
+      f.js   { render partial: 'search_results' }
+      f.json { render json:    @companies.to_json }
+    end
   end
-
+  
   def show
-  	begin
+    begin
       @company = Company.find_by_username(params[:company])
-  		render template: "companies/#{params[:company]}/home"
-  	rescue ActiveRecord::RecordNotFound, ActionView::MissingTemplate
-  		render file: Rails.root.join('public/404.html'), layout: false, status: 404
-  	end
+      render template: "companies/#{params[:company]}/home"
+    rescue ActiveRecord::RecordNotFound, ActionView::MissingTemplate
+      render file: Rails.root.join('public/404.html'), layout: false, status: 404
+    end
   end
 
   def fix_phone
