3c14be4c54965563f31b2d7f7f8d3ef7c33b0793
Felipe Bazzarella <fbazzarella@gmail.com>
2013-09-29 12:13:53 -0300

    add yui compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,10 @@ SirvaMe::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  # Compress JavaScripts and CSS
+  config.assets.compress       = true
+  config.assets.js_compressor  = :yui
+  config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
